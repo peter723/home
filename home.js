f56232269e7ede6b8eb8a2290448bd9be883a821
peter723 <90604780+peter723@users.noreply.github.com>
2022-05-02 07:17:15 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+let img= document.querySelector('.img')
+let containers=document.querySelector('.container')
+function phones(phone){
+  img.src = phone
+}
+function colors(color){
+containers.style.background=color;
+}
